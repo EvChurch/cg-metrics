d743a23fed8e343dbcb4cf326e0f6b90e84e7612
serenalau-ev <serena.lau@aucklandev.co.nz>
2025-09-22 19:10:33 +1200

    fix: prod url

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/pathways/" : "/",
+  base: process.env.NODE_ENV === 'production' ? '/cg-metrics/' : '/',
   build: {
-    outDir: "dist",
-    assetsDir: "assets",
+    outDir: 'dist',
+    assetsDir: 'assets',
     sourcemap: true,
     rollupOptions: {
       input: {
-        index: "index.html",
-        test: "test.html",
+        index: 'index.html',
+        test: 'test.html',
       },
     },
   },
