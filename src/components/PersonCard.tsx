796004b7d4bd73de244e391eead1272fa1d89556
serenalau-ev <serena.lau@aucklandev.co.nz>
2025-11-04 23:30:33 +1300

    fix: smooth scroll use parent

--- src/components/PersonCard.tsx
+++ src/components/PersonCard.tsx
@@ -91,9 +91,11 @@ const PersonCard = ({ personAttendance }: PersonCardProps) => {
               //     behavior: "smooth",
               //   });
               // }
-              document.getElementById("individual-attendance")?.scrollIntoView({
-                behavior: "smooth",
-              });
+              window.parent.document
+                .getElementById("individual-attendance")
+                ?.scrollIntoView({
+                  behavior: "smooth",
+                });
               setSelectedPerson(personAttendance);
             }}
             className="inline-flex items-center justify-center rounded-full border-2 border-[#E22A30] px-5 py-2 text-sm font-medium text-[#E22A30] hover:bg-red-50 hover:!text-[#E22A30] focus:outline-none focus:ring-2 focus:ring-red-500/50 cursor-pointer">
